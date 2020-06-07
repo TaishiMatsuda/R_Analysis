0ac46afac837a2df656bebe0118c174b5884e484
TaishiMatsuda <61358849+TaishiMatsuda@users.noreply.github.com>
2020-06-07 00:24:26 -0300

    Update Calculation Results

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,9 +3,9 @@ mpg_data <- read.csv('MechaCar_mpg.csv')
 coil_data <- read.csv('Suspension_Coil.csv')
 
 # Generate Multiple Linear Regression Model
-lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD, data=mpg_data)
+mpg_model = lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD, data=mpg_data)
 # Generate Summary Statistics for mpg
-summary(lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD, data=mpg_data))
+summary(mpg_model)
 
 # Create Summary Table for Suspension Coil Test Results
 summarize_coil <- coil_data %>% summarize(Max=max(PSI),Min=min(PSI),Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),StandardDeviation=sd(PSI))
